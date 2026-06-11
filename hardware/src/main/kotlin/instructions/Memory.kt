80912e3230da8531ad8a71b448fdb74271fec66b
Adam Mathlay <mathlayadam@icloud.com>
2026-06-11 19:37:58 +0200

    Literal

--- hardware/src/main/kotlin/instructions/Memory.kt
+++ hardware/src/main/kotlin/instructions/Memory.kt
@@ -1,1 +1,10 @@
 package io.cuttlefish.instructions
+
+import io.cuttlefish.*
+import io.cuttlefish.components.*
+
+suspend fun Cpu.handlerLit(instruction: Instruction.Lit) {
+    val destination = instruction.destination
+    val value = instruction.value
+    registers.write(destination, value)
+}
