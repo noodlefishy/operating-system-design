c1e81fa269fae7011f81f36db259a612f4c2c64f
Adam Mathlay <mathlayadam@icloud.com>
2026-06-11 16:00:48 +0200

    project setup

---
+++ compiler/src/main/kotlin/Main.kt
@@ -0,0 +1,4 @@
+package io.cuttlefish
+fun main() {
+
+}
---
+++ terminal/build.gradle.kts
@@ -0,0 +1,27 @@
+
+plugins {
+    kotlin("jvm")
+    application
+}
+
+group = "io.cuttlefish"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":hardware"))
+    implementation(project(":kernel"))
+    implementation(project(":compiler"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+    testImplementation(kotlin("test"))
+}
+application {
+    mainClass.set("Operating-System.terminal.MainKt") // Tells gradle where your main() function is
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
