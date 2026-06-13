acbddf434c47678bebc5b34897e368f53e5b7f53
Adam Mathlay <mathlayadam@icloud.com>
2026-06-13 13:33:09 +0200

    Fetch - decode - execute (not pipelined)

--- compiler/src/main/kotlin/InstructionType.kt
+++
@@ -1,6 +0,0 @@
-package io.cuttlefish
-
-enum class InstructionType {
-    Add, Addi, Nand, Lui, Lw, Sw, Beq, Jalr, Nop, Halt, LLi, Movi
-
-}
--- terminal/src/main/kotlin/Main.kt
+++ terminal/src/main/kotlin/Main.kt
@@ -1,18 +1,30 @@
 package io.cuttlefish
 
+import io.cuttlefish.backend.Backend
 import io.cuttlefish.components.*
 import io.cuttlefish.components.devices.*
-import java.io.*
+import java.io.File
 
 suspend fun main() {
     val parser = Parser(File("main.kar"))
+    val instructions = parser.decode()
+
+    val backend = Backend()
+    val machineCode = backend.encode(instructions)
+
     val memory = MemoryBus(
         PhysicalMemory(1024),
         DisplayDevice()
     )
+
+    for ((index, word) in machineCode.withIndex()) {
+        memory.write(index.toShort(), word.toShort())
+    }
+
     val cpu = Cpu(memory)
-    for (line in parser.decode()) {
-//        println(line)
-        cpu.tick(line)
+
+    // 3. Realistic Execution Loop: Run until a halt condition triggers
+    while (!cpu.isHalted) {
+        cpu.tick()
     }
 }
