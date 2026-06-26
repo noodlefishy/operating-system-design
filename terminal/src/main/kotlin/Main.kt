8832b5560cfc1611001e43ccdec39173894a094d
Needy Inky <needyinky@gmail.com>
2026-06-26 22:09:48 +0200

    run test?

--- terminal/src/main/kotlin/Main.kt
+++ terminal/src/main/kotlin/Main.kt
@@ -1,16 +1,13 @@
 package io.cuttlefish
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import io.cuttlefish.components.*
+import kotlinx.coroutines.*
+import java.io.*
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+    val parser = Parser(File("main.kar"))
+    val cpu = Cpu {}
+    for (line in parser.decode()) {
+        runBlocking { cpu.tick(line) }
     }
 }
