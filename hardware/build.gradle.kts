32c8df99a8445184ae07d7df1fec9a6d79fb3927
Needy Inky <needyinky@gmail.com>
2026-06-26 22:09:40 +0200

    project setup

---
+++ hardware/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "io.cuttlefish"
+version = "1.0-h"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
