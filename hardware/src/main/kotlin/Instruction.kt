775a0f1e359c31848dd951bf9dde803c3875e821
Needy Inky <needyinky@gmail.com>
2026-06-26 22:09:45 +0200

    Literal

--- hardware/src/main/kotlin/Instruction.kt
+++ hardware/src/main/kotlin/Instruction.kt
@@ -5,8 +5,7 @@ sealed interface Instruction {
     data class Sub(val number1: RegisterType, val number2: RegisterType, val destination: RegisterType) : Instruction
     data class Mul(val number1: RegisterType, val number2: RegisterType, val destination: RegisterType) : Instruction
     data class Div(val number1: RegisterType, val number2: RegisterType, val destination: RegisterType) : Instruction
-
-    data class Lit(val dest: RegisterType, val value: Long) : Instruction
+    data class Lit(val destination: RegisterType, val value: Short) : Instruction
     data class Syscall(val id: RegisterType, val arg1: RegisterType, val arg2: RegisterType) : Instruction
     data object Halt : Instruction
 }
