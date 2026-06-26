4329ff3082c305c3e227f83370b8f6edb8f4c5ab
Needy Inky <needyinky@gmail.com>
2026-06-26 22:09:59 +0200

    correct incorrect JALR

--- hardware/src/main/kotlin/Instruction.kt
+++ hardware/src/main/kotlin/Instruction.kt
@@ -1,12 +1,12 @@
 package io.cuttlefish
 
 sealed interface Instruction {
-    data class Add(val register1: RegisterType, val register2: RegisterType, val register3: RegisterType): Instruction
-    data class Addi(val register1: RegisterType, val register2: RegisterType, val immediate: Short): Instruction
-    data class Nand(val register1: RegisterType, val register2: RegisterType, val register3: RegisterType): Instruction
-    data class Lui(val register1: RegisterType, val immediate: Short): Instruction
-    data class Lw(val register1: RegisterType, val register2: RegisterType, val immediate: Short): Instruction
-    data class Sw(val register1: RegisterType, val register2: RegisterType, val immediate: Short): Instruction
-    data class Beq(val register1: RegisterType, val register2: RegisterType, val immediate: Short): Instruction
-    data class Jalr(val register1: RegisterType, val register2: RegisterType): Instruction
+    data class Add(val register1: RegisterType, val register2: RegisterType, val register3: RegisterType) : Instruction
+    data class Addi(val register1: RegisterType, val register2: RegisterType, val immediate: Short) : Instruction
+    data class Nand(val register1: RegisterType, val register2: RegisterType, val register3: RegisterType) : Instruction
+    data class Lui(val register1: RegisterType, val immediate: Short) : Instruction
+    data class Lw(val register1: RegisterType, val register2: RegisterType, val immediate: Short) : Instruction
+    data class Sw(val register1: RegisterType, val register2: RegisterType, val immediate: Short) : Instruction
+    data class Beq(val register1: RegisterType, val register2: RegisterType, val immediate: Short) : Instruction
+    data class Jalr(val register1: RegisterType, val register2: RegisterType, val immediate: Short) : Instruction
 }
