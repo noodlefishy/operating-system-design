3ec6187882b2c7c53f93033c37ba30973b932b99
Needy Inky <needyinky@gmail.com>
2026-06-26 22:09:49 +0200

    Added pseudos to Parser!!

--- compiler/src/main/kotlin/Parser.kt
+++ compiler/src/main/kotlin/Parser.kt
@@ -3,6 +3,7 @@ package io.cuttlefish
 import io.cuttlefish.InstructionType.*
 import java.io.*
 import java.util.Locale.getDefault
+import kotlin.experimental.*
 
 class Parser(val file: File) {
     val text = file.readLines()
@@ -12,33 +13,43 @@ class Parser(val file: File) {
         for (line in text) {
             val parts = line.split(" ")
             if (parts.isEmpty()) continue
-            val type = InstructionType.valueOf(
+            val type = valueOf(
                 parts[0].lowercase()
                     .replaceFirstChar { if (it.isLowerCase()) it.titlecase(getDefault()) else it.toString() })
 
+            when (type) {
+                Add -> {
+                    instructions += Instruction.Add(
+                        register1 = parts[1].toRegisterType(),
+                        register2 = parts[2].toRegisterType(),
+                        register3 = parts[3].toRegisterType()
+                    )
+                }
 
-            val curr = when (type) {
-                Add -> Instruction.Add(parts[1].toRegisterType(), parts[2].toRegisterType(), parts[3].toRegisterType())
                 Addi -> {
                     val imm = parts[3].toShort()
                     if (imm !in -64..63) {
                         throw Exception("Assembler Error: Immediate $imm out of range for ADDI (-64 to 63)")
                     }
-                    Instruction.Addi(
+                    instructions += Instruction.Addi(
                         register1 = parts[1].toRegisterType(), register2 = parts[2].toRegisterType(), immediate = imm
                     )
                 }
 
-                Nand -> Instruction.Nand(
-                    parts[1].toRegisterType(), parts[2].toRegisterType(), parts[3].toRegisterType()
-                )
+                Nand -> {
+                    instructions += Instruction.Nand(
+                        register1 = parts[1].toRegisterType(),
+                        register2 = parts[2].toRegisterType(),
+                        register3 = parts[3].toRegisterType()
+                    )
+                }
 
                 Lui -> {
                     val imm = parts[3].toShort()
                     if (imm !in 0..1023) {
                         throw Exception("Assembler Error: Immediate $imm out of range for LUI (0 to 1023)")
                     }
-                    Instruction.Lui(
+                    instructions += Instruction.Lui(
                         register1 = parts[1].toRegisterType(), immediate = imm
                     )
                 }
@@ -48,7 +59,7 @@ class Parser(val file: File) {
                     if (imm !in -64..63) {
                         throw Exception("Assembler Error: Immediate $imm out of range for LW (-64 to 63)")
                     }
-                    Instruction.Lw(
+                    instructions += Instruction.Lw(
                         register1 = parts[1].toRegisterType(), register2 = parts[2].toRegisterType(), immediate = imm
                     )
                 }
@@ -58,7 +69,7 @@ class Parser(val file: File) {
                     if (imm !in -64..63) {
                         throw Exception("Assembler Error: Immediate $imm out of range for SW (-64 to 63)")
                     }
-                    Instruction.Sw(
+                    instructions += Instruction.Sw(
                         register1 = parts[1].toRegisterType(), register2 = parts[2].toRegisterType(), immediate = imm
                     )
                 }
@@ -68,7 +79,7 @@ class Parser(val file: File) {
                     if (imm !in -64..63) {
                         throw Exception("Assembler Error: Immediate $imm out of range for BEQ (-64 to 63)")
                     }
-                    Instruction.Beq(
+                    instructions += Instruction.Beq(
                         register1 = parts[1].toRegisterType(), register2 = parts[2].toRegisterType(), immediate = imm
                     )
                 }
@@ -78,15 +89,58 @@ class Parser(val file: File) {
                     if (imm !in -64..63) {
                         throw Exception("Assembler Error: Immediate $imm out of range for JALR (-64 to 63)")
                     }
-                    Instruction.Jalr(
+                    instructions += Instruction.Jalr(
                         register1 = parts[1].toRegisterType(), register2 = parts[2].toRegisterType(), immediate = imm
                     )
                 }
+                // Pseudos!
+                Nop -> {
+                    instructions += Instruction.Add(
+                        register1 = RegisterType.RZ, register2 = RegisterType.RZ, register3 = RegisterType.RZ
+                    )
+                }
+
+                Halt -> {
+                    instructions += Instruction.Jalr(
+                        register1 = RegisterType.RZ, register2 = RegisterType.RZ, immediate = 1
+                    )
+                }
+
+                LLi -> {
+                    // addi regA, regA, (imm & 0x3F)
+                    val imm = parts[3].toShort()
+                    if (imm !in -64..63) {
+                        throw Exception("Assembler Error: Immediate $imm out of range for JALR (-64 to 63)")
+                    }
+                    instructions += Instruction.Addi(
+                        register1 = parts[1].toRegisterType(), register2 = parts[1].toRegisterType(), immediate = imm
+                    )
+
+                }
+
+                Movi -> {
+
+                    val imm = parts[3].toShort()
+                    if (imm !in Short.MIN_VALUE..Short.MAX_VALUE) {
+                        throw Exception("Assembler Error: Immediate $imm out of range for Movi (${Short.MIN_VALUE} to ${Short.MAX_VALUE})")
+                    }
+                    val lower6Mask: Short = 0x3F
+                    val lui = (imm.toInt() shr 6).toShort()
+                    val lli = (imm and lower6Mask)
+                    // movi RegA imm
+                    instructions += Instruction.Lui(
+                        register1 = parts[1].toRegisterType(), immediate = lui
+                    )
+                    instructions += Instruction.Addi( // pseudo of LLI
+                        register1 = parts[1].toRegisterType(), register2 = parts[1].toRegisterType(), immediate = lli
+                    )
+
+                }
+
+
             }
 
 
-
-            instructions.add(curr)
         }
         return instructions.toList()
     }
