4d39c123d9975012cacccee15aba1c1094340a51
Needy Inky <needyinky@gmail.com>
2026-06-26 22:09:51 +0200

    removing cli

--- terminal/src/main/kotlin/Main.kt
+++ terminal/src/main/kotlin/Main.kt
@@ -1,32 +1,41 @@
 package io.cuttlefish
 
-import io.cuttlefish.backend.*
-import io.cuttlefish.components.*
-import io.cuttlefish.components.devices.*
-import java.io.*
+// lc -c $file -o $out (compile)
+// lc -i $file (compile + run)
+// lc -r $file (run)
+suspend fun main(args: Array<String>) {
+    when (args[1]) { // flag
+        "-c" -> {
 
-suspend fun main() {
-    val parser = Parser(File("main.kar"))
-    val instructions = parser.decode()
+        }
 
-    val backend = Backend()
-    val machineCode = backend.encode(instructions)
-    println(machineCode)
-//    backend.decode(machineCode).forEach(::println)
-
-    val memory = MemoryBus(
-        PhysicalMemory(1024),
-        DisplayDevice()
-    )
-
-    for ((index, word) in machineCode.withIndex()) {
-        memory.write(index.toShort(), word.toShort())
     }
 
-    val cpu = Cpu(memory)
-
-    while (!cpu.isHalted) {
-        cpu.tick()
-    }
-    println(cpu.registers)
 }
+
+
+//suspend fun main() {
+//    val parser = Parser(File("main.kar"))
+//    val instructions = parser.decode()
+//
+//    val backend = Backend()
+//    val machineCode = backend.encode(instructions)
+//    println(machineCode)
+////    backend.decode(machineCode).forEach(::println)
+//
+//    val memory = MemoryBus(
+//        PhysicalMemory(1024),
+//        DisplayDevice()
+//    )
+//
+//    for ((index, word) in machineCode.withIndex()) {
+//        memory.write(index.toShort(), word.toShort())
+//    }
+//
+//    val cpu = Cpu(memory)
+//
+//    while (!cpu.isHalted) {
+//        cpu.tick()
+//    }
+//    println(cpu.registers)
+//}
