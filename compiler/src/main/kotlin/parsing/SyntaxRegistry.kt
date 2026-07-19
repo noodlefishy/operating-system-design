c0f1c54c18319eca4c30736e1dd711b252240730
Needy Inky <needyinky@gmail.com>
2026-07-19 08:41:45 +0200

    updated SyntaxRegistry.kt

--- compiler/src/main/kotlin/parsing/SyntaxRegistry.kt
+++ compiler/src/main/kotlin/parsing/SyntaxRegistry.kt
@@ -7,8 +7,14 @@ class SyntaxRegistry {
     val rules = mutableListOf<TokenRule>()
 
     init {
+        // DO NOT ever change this order ✨
         rules += SkipTokenRule()
+        rules += StringLiteralTokenRule()
         rules += LabelTokenRule()
+        rules += RegisterTokenRule()
+        rules += ImmediateTokenRule()
+        rules += MnemonicTokenRule()
+        rules += SymbolReferenceTokenRule()
 
         val loader = ServiceLoader.load(TokenRule::class.java)
         for (otherRule in loader) {
